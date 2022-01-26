a0a2b4db9cef852ae2cc262e132d2654e0c1e107
unknown <zenica.filip@gmail.com>
2022-01-26 20:38:46 +0100

    first

---
+++ src/components/ZEBus.vue
@@ -0,0 +1,43 @@
+<template>
+  
+  <div class="container">
+    <div class="row">
+
+      <div class="col-12">
+        <h1>Lista linija</h1>
+      </div>
+      <!-- /.col-12 -->
+
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+
+</template>
+
+<script>
+export default {
+  name: 'ZEBus',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
